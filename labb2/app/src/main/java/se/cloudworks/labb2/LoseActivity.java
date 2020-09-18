399d2956d200984b7ecfd504388bf2009d62c9ff
Andreas Hortlund <hortlund.andreas@gmail.com>
2020-09-18 19:40:55 +0200

    Work in progress

---
+++ labb2/app/src/main/java/se/cloudworks/labb2/LoseActivity.java
@@ -0,0 +1,13 @@
+package se.cloudworks.labb2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoseActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lose);
+    }
+}
---
+++ labb2/app/src/main/java/se/cloudworks/labb2/WinActivity.java
@@ -0,0 +1,14 @@
+package se.cloudworks.labb2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WinActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.win);
+    }
+
+}
