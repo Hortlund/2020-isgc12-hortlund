ac6c0e4ad60bbfd8a8ba0b217e74ca4db80701fc
Andreas Hortlund <hortlund.andreas@gmail.com>
2020-10-02 13:28:55 +0200

    Almost done

--- labb3/app/src/main/java/se/cloudworks/labb3/ApiCall.java
+++ labb3/app/src/main/java/se/cloudworks/labb3/ApiCall.java
@@ -3,7 +3,6 @@ package se.cloudworks.labb3;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Parcelable;
 import android.util.Log;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -11,15 +10,13 @@ import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
-
 public class ApiCall extends AsyncTask<String, Void, ArrayList> {
     private Context _ctx;
     private ArrayList<String> similarArtists = new ArrayList<>();
     private URL url;
+    private String name;
 
     public ApiCall(Context ctx){
         _ctx = ctx;
@@ -28,8 +25,9 @@ public class ApiCall extends AsyncTask<String, Void, ArrayList> {
     @Override
     protected ArrayList<String> doInBackground(String... artist) {
         try {
+            name = artist[0];
             similarArtists = new ArrayList<>();
-            url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+artist[0]+"&api_key=YOUR_API_KEY");
+            url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+name+"&api_key=YOUR_API_KEY");
             XmlPullParserFactory factory = null;
             factory = XmlPullParserFactory.newInstance();
             XmlPullParser parser = factory.newPullParser();
@@ -41,24 +39,31 @@ public class ApiCall extends AsyncTask<String, Void, ArrayList> {
                 if(parserEvent == XmlPullParser.START_TAG){
                     tagName = parser.getName();
                     Log.d("walla", "Start tag found: "+tagName);
+                    if(tagName.equals("error")){
+                        similarArtists.add(parser.nextText());
+                    }
                     if(tagName.equals("name")){
-                        similarArtists.add(parser.getAttributeType(0));
+                        similarArtists.add(parser.nextText());
                     }
                 }
-                //Log.d("walla", String.valueOf(parserEvent));
+
                 parserEvent = parser.next();
             }
 
         } catch (XmlPullParserException | IOException e) {
             e.printStackTrace();
         }
-
+        Log.d("walla", similarArtists.toString());
         return similarArtists;
     }
 
     @Override
     protected void onPostExecute(ArrayList similarArtists) {
         super.onPostExecute(similarArtists);
-        //Log.d("walla", similarArtists.toString());
+        Intent intent = new Intent("se.cloudworks.MainActivity");
+        intent.putExtra("name", name.replace("%20", " "));
+        intent.putStringArrayListExtra("list",similarArtists);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        _ctx.startActivity(intent);
     }
 }
--- labb3/app/src/main/java/se/cloudworks/labb3/MainActivity.java
+++ labb3/app/src/main/java/se/cloudworks/labb3/MainActivity.java
@@ -3,30 +3,44 @@ package se.cloudworks.labb3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String artistName;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void onClick(View view){
         EditText artistSearch = findViewById(R.id.artistSearch);
-
         ApiCall apiCall = new ApiCall(getApplicationContext());
-        apiCall.execute(artistSearch.getText().toString().replace(' ', '+'));
+        artistName = artistSearch.getText().toString();
+        if(artistName.matches("")){
+
+        }else{
+            apiCall.execute(artistName.replace(" ", "%20"));
+        }
+
     }
 
     public void onResume() {
         super.onResume();
+        if (getIntent().getStringArrayListExtra("list") != null) {
+            TextView searchedArtist = findViewById(R.id.textView2);
+            searchedArtist.setText("Artist similar to: " + getIntent().getStringExtra("name") );
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getIntent().getStringArrayListExtra("list"));
+            ListView lw = findViewById(R.id.listview1);
+            lw.setAdapter(adapter);
+        }
+
 
     }
 
