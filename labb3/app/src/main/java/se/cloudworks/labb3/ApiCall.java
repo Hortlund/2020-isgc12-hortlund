d9c89608a7a610f0dd2beb205a27f66846d03f9b
Andreas Hortlund <hortlund.andreas@gmail.com>
2020-10-05 10:29:56 +0200

    labb3 done

--- labb3/app/src/main/java/se/cloudworks/labb3/ApiCall.java
+++ labb3/app/src/main/java/se/cloudworks/labb3/ApiCall.java
@@ -14,11 +14,13 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 public class ApiCall extends AsyncTask<String, Void, ArrayList> {
+    //Declaring variables and data structures
     private Context _ctx;
     private ArrayList<String> similarArtists = new ArrayList<>();
     private URL url;
     private String name;
 
+    //Constructor of class that takes the application context of main activity
     public ApiCall(Context ctx){
         _ctx = ctx;
     }
@@ -26,43 +28,65 @@ public class ApiCall extends AsyncTask<String, Void, ArrayList> {
     @Override
     protected ArrayList<String> doInBackground(String... artist) {
         try {
+            //Since asyncTask takes a array of strings as parameter, take out the first one(only one)
             name = artist[0];
+            //Create a new list for every search
             similarArtists = new ArrayList<>();
+            //Encode the search term to be urlsafe
             String urlSafe = URLEncoder.encode(name, "utf-8");
+            //new url object
             url = new URL("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+urlSafe+"&api_key=YOUR_API_KEY");
+            //create a xml factory
             XmlPullParserFactory factory = null;
+            //initialize
             factory = XmlPullParserFactory.newInstance();
+            //new parser
             XmlPullParser parser = factory.newPullParser();
+            //ser url stream as input
             parser.setInput(url.openStream(), null);
+            //get parser event
             int parserEvent = parser.getEventType();
+            //For storing the tag name
             String tagName;
+            //check that we are not at the end of document
             while(parserEvent != XmlPullParser.END_DOCUMENT){
+                //if we are at a start tag
                 if(parserEvent == XmlPullParser.START_TAG){
+                    //get tag name
                     tagName = parser.getName();
+                    //compare and check for errors and add them tho the list, there are more errors to check for, but this will due for now
                     if(tagName.equals("error")){
                         similarArtists.add(parser.nextText());
                     }
+                    //if we get tag name "name", get the text and add to list
                     if(tagName.equals("name")){
                         similarArtists.add(parser.nextText());
                     }
                 }
-
+                //continue trough next tag
                 parserEvent = parser.next();
             }
-
+            //check for xml or IO exception
         } catch (XmlPullParserException | IOException e) {
             e.printStackTrace();
         }
+        //return array list
         return similarArtists;
     }
 
     @Override
+    //runs after doInbackrgound, parameter is the list.
     protected void onPostExecute(ArrayList similarArtists) {
         super.onPostExecute(similarArtists);
+        //new intent to get back to mainactivity
         Intent intent = new Intent("se.cloudworks.MainActivity");
+        //Return artist name to display the search term
         intent.putExtra("name", name);
+        //Return list
         intent.putStringArrayListExtra("list",similarArtists);
+        //Flag to start the activity as new task
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //GO! :D
         _ctx.startActivity(intent);
     }
 }
--- labb3/app/src/main/java/se/cloudworks/labb3/MainActivity.java
+++ labb3/app/src/main/java/se/cloudworks/labb3/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Declaring variable & data structures
     private String artistName;
     private ArrayAdapter adapter;
     @Override
@@ -20,19 +21,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view){
+        //get text from search field.
         EditText artistSearch = findViewById(R.id.artistSearch);
+        //New instance of apicall class and create it with application context
         ApiCall apiCall = new ApiCall(getApplicationContext());
+        //Stringify the text
         artistName = artistSearch.getText().toString();
+        //Execute the asynchronous task
         apiCall.execute(artistName);
     }
 
     public void onResume() {
         super.onResume();
+        //If we return with a list of artist go go forth
         if (getIntent().getStringArrayListExtra("list") != null) {
+            //Get text field reference
             TextView searchedArtist = findViewById(R.id.textView2);
+            //Set the text to the artist we searched for.
             searchedArtist.setText("Artist similar to: " + getIntent().getStringExtra("name") );
+            //A new array adapter so we can use the array list as a data source and display it in the UI, simple_list_item_1 is a android native reference for a list ui element.
             adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getIntent().getStringArrayListExtra("list"));
+            //Get reference of listview
             ListView lw = findViewById(R.id.listview1);
+            //connect the adapter and list view
             lw.setAdapter(adapter);
         }
 
