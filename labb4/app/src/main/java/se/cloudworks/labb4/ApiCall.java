cc175435e6cb37732483fef2c8d8bff9219d125d
Andreas Hortlund <hortlund.andreas@gmail.com>
2020-10-08 13:52:29 +0200

    started on layouts

---
+++ labb4/app/src/main/java/se/cloudworks/labb4/ApiCall.java
@@ -0,0 +1,15 @@
+package se.cloudworks.labb4;
+
+import android.os.AsyncTask;
+
+public class ApiCall extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+}
