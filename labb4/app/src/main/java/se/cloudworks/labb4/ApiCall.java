3971e0ef4902da910e08cbd2340b22e960f7b7f4
Andreas Hortlund <hortlund.andreas@gmail.com>
2020-10-11 18:26:56 +0200

    some work done

--- labb4/app/src/main/java/se/cloudworks/labb4/ApiCall.java
+++ labb4/app/src/main/java/se/cloudworks/labb4/ApiCall.java
@@ -1,15 +1,55 @@
 package se.cloudworks.labb4;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 
-public class ApiCall extends AsyncTask {
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.ArrayList;
+
+public class ApiCall extends AsyncTask<String, Void, ArrayList>{
+
+    private Context _ctx;
+    private ArrayList<String> movieSearch = new ArrayList<>();
+
+    public ApiCall(Context ctx){
+        _ctx = ctx;
+    }
     @Override
-    protected Object doInBackground(Object[] objects) {
-        return null;
+    protected ArrayList<String> doInBackground(String... movies) {
+
+        RequestQueue queue;
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(_ctx.getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        queue = new RequestQueue(cache, network);
+
+        // Start the queue
+        queue.start();
+
+
+
+        movieSearch = new ArrayList<>();
+        return movieSearch;
     }
 
     @Override
-    protected void onPostExecute(Object o) {
-        super.onPostExecute(o);
+    protected void onPostExecute(ArrayList movies) {
+        super.onPostExecute(movies);
+        Intent intent = new Intent("se.cloudworks.MainActivity");
     }
 }
--- labb4/app/src/main/java/se/cloudworks/labb4/MainActivity.java
+++ labb4/app/src/main/java/se/cloudworks/labb4/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String movieTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClick(View view){
+        EditText search = findViewById(R.id.movieSearch);
+        ApiCall apiCall = new ApiCall(getApplicationContext());
+        movieTitle = search.getText().toString();
+        apiCall.execute(movieTitle);
 
+
+    }
+
+    public void onResume() {
+        super.onResume();
     }
 }
